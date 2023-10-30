4d1b2be976fb954dac8dbec64f9b8ae712081e91
Ramesh Bulla <148524062+Rameshbulla01@users.noreply.github.com>
2023-10-30 13:55:09 +0530

    Add files via upload

---
+++ JS-Datatypes/index.js
@@ -0,0 +1,53 @@
+// in the js we have differnt data tyeps avilable
+// Primitive data types
+// 1.numbers
+// let  a = 10;
+// console.log(a);
+// console.log(typeof a);
+
+// 2.string it is sequence of a charecters which are written quots
+// let b = "name";
+// console.log(typeof b);
+// console.log(b);
+
+// 3.Boolean
+// true and false
+// let c = true;
+// console.log(c);
+// console.log(typeof c);
+
+// 4.Undifined
+// if you not difined any value then js will automatically calls it was undifined
+// let d;
+// console.log(d);
+// console.log(typeof d);
+
+// Null
+//  5.Null means empty
+// let e = null;
+// console.log(e);
+// console.log(typeof e);
+
+// pause by the value
+// let a = 10;
+// let b = a;
+// console.log(b);
+// console.log(a);
+// b = b+1;
+// console.log(b);
+// console.log (a);  
+
+// pause by reference
+// let c = [1,2];
+// let e = c;
+// console.log(c);
+// console.log(e);
+// e.push(3);
+// console.log(e);
+// console.log(c);  //it wiil chnage the entire data
+
+// Arrays
+// let information = ["stydent1","student2","stundent3"];
+// console.log(information);
+// console.log(information[2]);
+// console.log(information[1]);
